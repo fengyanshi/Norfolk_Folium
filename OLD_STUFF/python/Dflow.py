cb6a99597df93cda212de53b5e4e2e63460d9878
fengyanshi <fyshi.gbox@gmail.com>
2024-08-13 15:03:18 -0400

    move old stuff

--- OLD_STUFF/python/Dflow.py
+++ OLD_STUFF/python/Dflow.py
@@ -15,19 +15,19 @@ from folium.plugins import MousePosition
 # In[13]:
 
 
-flood_NF=os.path.join('/Users/fengyanshi15/proposal/JACK_2020/TWL/interactive/matlab','hmax_71.png')
-hmax_NF=os.path.join('/Users/fengyanshi15/proposal/JACK_2020/TWL/interactive/matlab','hmax_71.png')
+flood_NF=os.path.join('../processing','hmax_71.png')
+hmax_NF=os.path.join('../processing','hmax_71.png')
 #flood_PR=os.path.join('/Users/fengyanshi15/work/NTHMP/Flood/VB_2','PR_flood.png')
 #flood_CVV80=os.path.join('/Users/fengyanshi15/work/NTHMP/Flood/VB_2','CVV80_flood.png')
 #flood_COMB=os.path.join('/Users/fengyanshi15/work/NTHMP/Flood/VB_2','COMB_flood.png')
 #fld_bar=os.path.join('/Users/fengyanshi15/work/NTHMP/colorbar','Colorbar_0_1.png')
 
-m=folium.Map([36.935092999999998,-76.299999999999994],zoom_start=10.2,control_scale=True)
+m=folium.Map([36.95,-76.291],zoom_start=14.2,control_scale=True)
 img1=folium.raster_layers.ImageOverlay(
     name='Flood Area',
     image=flood_NF,
 #     bounds=[[36.461,-76.211],[36.660722,-75.811278]],
-    bounds=[[36.635092999999998,-76.599999999999994],[37.190648000000003,-75.998240999999993]],
+    bounds=[[36.930,-76.34],[36.970,-76.245]],
     opacity=0.7,
     interactive=True,
     cross_origin=False,
@@ -39,7 +39,7 @@ img1.add_to(m)
 img2=folium.raster_layers.ImageOverlay(
     name='Max Elevation',
     image=hmax_NF,
-    bounds=[[36.635092999999998,-76.599999999999994],[37.190648000000003,-75.998240999999993]],
+    bounds=[[36.930,-76.34],[36.970,-76.245]],
     opacity=0.7,
     interactive=True,
     cross_origin=False,
@@ -75,7 +75,7 @@ kw = {
 }
 
 folium.Rectangle(
-    bounds=[[36.635092999999998,-76.599999999999994],[37.190648000000003,-75.998240999999993]],
+    bounds=[[36.930,-76.34],[36.970,-76.245]],
     line_join='round',
     dash_array='5, 5',
     **kw,
